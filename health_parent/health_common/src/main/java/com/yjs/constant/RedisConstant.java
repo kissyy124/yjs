43dd725abdd59ecfd87df044ece97f19328709cf
admin <kissyy124@qq.com>
2019-12-16 10:22:19 +0800

    TESE2

--- health_parent/health_common/src/main/java/com/yjs/constant/RedisConstant.java
+++ health_parent/health_common/src/main/java/com/yjs/constant/RedisConstant.java
@@ -2,7 +2,7 @@ package com.yjs.constant;
 
 public class RedisConstant {
     //套餐图片所有图片名称
-    public static final String SETMEAL_PIC_RESOURCES = "setmealPicResourcesABC";
+    public static final String SETMEAL_PIC_RESOURCES = "setmealPicResourcesABCzz";
     //套餐图片保存在数据库中的图片名称
     public static final String SETMEAL_PIC_DB_RESOURCES = "setmealPicDbResources";
 }
